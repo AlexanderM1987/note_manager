e82bb25a0fd71011e492be91eca40b5a39f4603d
AlexanderM1987 <5ak_86@mail.ru>
2025-01-18 19:43:27 +0000

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+username='Александр'
+title='Заметка 1'
+content='Создание заметки 1'
+status='Создана'
+created_date='18-01-2025'
+issue_date='31-01-2025'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date[0:5])
+print('Дата истечения заметки (дедлайн):', issue_date[0:5])
